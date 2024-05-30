99dea600021bee1627c3d0c4c41ad2305f9f9e98
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-05-30 16:19:12 +0200

    -prepare Model - Product - preparing CreateProductEndpoint and Handler - prepare BuildingBlocks project with MediatR ICommand, ICommandHandler, IQuery, IQueryHandler generics. The goal is to share common MediatR command/handler between the different Services (instead of implementing them in each service)

---
+++ src/Serrvices/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.API.Models;
+using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) 
+        : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //create Product entity from command object
+            //save to DB
+            //return CreateProductResult result
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+            return new CreateProductResult(Guid.NewGuid());
+            //throw new NotImplementedException();
+        }
+    }
+}
