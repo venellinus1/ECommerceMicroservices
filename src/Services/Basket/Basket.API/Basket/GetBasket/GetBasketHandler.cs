f50dd75fe72be12fcc94a923483f2c18e8614817
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-06-07 10:42:16 +0200

    Basket - add endpoints for Store, Get, Delete basket

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,17 @@
+﻿
+using Basket.API.Basket.Data;
+
+namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+public record GetBasketResult(ShoppingCart Cart);
+public class GetBaskeQuerytHandler(IBasketRepository repository)
+    : IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        //todo get basket from database
+        var basket = await repository.GetBasket(query.UserName);
+        return new GetBasketResult(basket);
+    }
+}
---
+++ src/Services/Basket/Basket.API/Program.cs
@@ -0,0 +1,25 @@
+using BuildingBlocks.Behaviours;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+builder.Services.AddMarten(options => 
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+    options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+var app = builder.Build();
+
+app.MapCarter();
+
+app.Run();
