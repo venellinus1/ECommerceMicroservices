0000f4d09a7917e38bd07267ef9fbcaf195324a3
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-06-10 18:16:08 +0200

    Ordering.Domain - add Abstractions, Enums, Events, Models, ValueObjects Ordering.Infrastructure - preparing Configurations

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,5 +1,17 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 //add services to the container
+//Infrastructure - EF Core
+//Application - MediatR
+//API - Carter, HealthChecks,...
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
 
 var app = builder.Build();
 
