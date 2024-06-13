6c08c5bef31c5bdd1a78f8735f624f2fbc9edb49
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-06-13 18:48:08 +0200

    add IApplicationDbContext add dependency injection in Ordering.Infrastructure for IApplicationDbContext

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,16 @@
+﻿
+using BuildingBlocks.CQRS;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public class CreateOrderHandler 
+    : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+{
+    public Task<CreateOrderResult> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
+    {
+        //create Order from command object
+        //save to DB
+        //return result
+        throw new NotFiniteNumberException();
+    }
+}
--- src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Data;
 using Ordering.Domain.Models;
 using System.Reflection;
 
 namespace Ordering.Infrastructure.Data;
-
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : DbContext, IApplicationDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options) { }
@@ -13,6 +13,7 @@ public class ApplicationDbContext : DbContext
     public DbSet<Product> Products => Set<Product>();
     public DbSet<Order> Orders => Set<Order>();
     public DbSet<OrderItem> OrderItems => Set<OrderItem>();
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
