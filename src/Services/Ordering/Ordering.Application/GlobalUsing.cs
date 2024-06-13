e65dbb021fa876dd4ecf74f0495ebf1e77b2939c
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-06-13 20:46:42 +0200

    Ordering.Application - added UpdateOrderHandler

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -1,2 +1,6 @@
 ﻿global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
+global using BuildingBlocks.CQRS;
+global using Ordering.Application.Data;
+global using Ordering.Application.DTOs;
+global using Ordering.Application.Exceptions;
