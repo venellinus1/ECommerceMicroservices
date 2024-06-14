afe273778a861148bfb04817bd53ab7b3c656d10
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-06-14 10:19:16 +0200

    Apply pagination to GetOrders

---
+++ src/BuildingBlocks/BuildingBlocks/Pagination/PaginatedResult.cs
@@ -0,0 +1,11 @@
+﻿namespace BuildingBlocks.Pagination;
+
+public class PaginatedResult<TEntity>
+    (int pageIndex, int pageSize, long count, IEnumerable<TEntity> data) 
+    where TEntity : class
+{
+    public int PageIndex { get; } = pageIndex;
+    public int PageSize { get; } = pageSize;
+    public IEnumerable<TEntity> Data { get; } = data;
+    public long Count { get; } = count;
+}
---
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerHandler.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+public class GetOrdersByCustomerHandler(IApplicationDbContext dbContext)
+    : IQueryHandler<GetOrdersByCustomerQuery, GetOrdersByCustomerResult>
+{
+    public async Task<GetOrdersByCustomerResult> Handle(GetOrdersByCustomerQuery query, CancellationToken cancellationToken)
+    {
+        //get orders by customer using dbcontext
+        //return result
+        var orders = await dbContext.Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .Where(o => o.CustomerId == CustomerId.Of(query.CustomerId))
+            .ToListAsync(cancellationToken);
+
+        return new GetOrdersByCustomerResult(orders.ToOrderDtoList());
+    }
+}
