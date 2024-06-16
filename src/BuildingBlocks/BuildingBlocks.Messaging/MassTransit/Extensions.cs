caace9e13a6f32ebee2cd9c05c95b1ff82fbea9d
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-06-16 17:20:55 +0200

    Add new project BuildingBlocks.Messaging Add folders for Events and MassTransit Install MassTransit

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.Messaging.MassTransit;
+
+public static class Extensions
+{
+    public static IServiceCollection AddMessageBroker(this IServiceCollection services, Assembly? assembly = null)
+    {
+        //todo implement RabbitMQ MassTransit configuration
+        return services;
+    }
+}
