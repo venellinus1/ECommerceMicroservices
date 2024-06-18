80953b4948907bdfa71e06e5ed6129163b79f0b9
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-06-18 16:07:52 +0200

    Setup appsettings.json for Yarp gateway to forward to catalog,basket,ordering services Add ratelimiting for Ordering Containerize

--- src/APIGateways/YarpAPIGateway/Program.cs
+++ src/APIGateways/YarpAPIGateway/Program.cs
@@ -1,11 +1,24 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
-//Add services to the container
-builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+// Add services to the container.
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(rateLimiterOptions => 
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options => 
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+});
 var app = builder.Build();
 
-//configure the http request pipeline
+// Configure the HTTP request pipeline
+
+app.UseRateLimiter();//important - call before mapreverseproxy
 app.MapReverseProxy();
 
 app.Run();
