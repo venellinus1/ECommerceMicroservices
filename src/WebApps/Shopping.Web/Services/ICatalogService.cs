3e636c6ba42aaa751e113c6b141fbe147bd53a9b
venellinus1 <34251748+venellinus1@users.noreply.github.com>
2024-06-20 09:59:33 +0200

    Shopping.Web Business layer: ICatalogService, IOrderingService,  IBasketService with Refit

--- src/WebApps/Shopping.Web/Services/ICatalogService.cs
+++ src/WebApps/Shopping.Web/Services/ICatalogService.cs
@@ -3,7 +3,12 @@ namespace Shopping.Web.Services;
 
 public interface ICatalogService
 {
+    [Get("/catalog-service/products?pageNumber={pageNumber}&pageSize={pageSize}")]
     Task<GetProductsResponse> GetProducts(int? pageNumber = 1, int? pageSize = 10);
+
+    [Get("/catalog-service/products/{id}")]
     Task<GetProductByIdResponse> GetProduct(Guid id);
-    Task<GetProductByCategoryResponse> etProductByCategory(string category);
+
+    [Get("/catalog-service/products/category/{category}")]
+    Task<GetProductByCategoryResponse> GetProductsByCategory(string category);
 }
